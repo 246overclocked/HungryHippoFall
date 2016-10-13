35f58743e1270f4d6ca1de7b3263f95a32c170f2
paulterrasi <paul.a.terrasi@gmail.com>
2016-10-13 11:51:24 -0400

    Create command-based project with JUnit testing and proper ReadMe.md and Contributing.md files

---
+++ src/org/usfirst/frc/team246/robot/RobotMap.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team246.robot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    
+	// Create constants and declare variables here
+	
+	public static void init() {
+		
+		// Instantiate and configure objects here
+		
+	}
+}
