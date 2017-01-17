c028005f944935f50a2cc548847ad15ad9477db7
edward <edwardstarsong@gmail.com>
2017-01-17 18:26:42 -0500

    Moved python client into the project

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket, sys
+
+HOST = "roboRIO-246-FRC.local"
+PORT = 8080
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+while(True):
+    
+    msg = raw_input("> ")
+    sock.sendall(msg + "\n")
+    
+    if(msg == "exit"):
+        sys.exit()
+
+    data = sock.recv(1024)
+    print data
+
+
+
